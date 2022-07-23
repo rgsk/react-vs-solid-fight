f09cd428f48d3eed172fda7729252d34c319c3fc
rgsk <guptarahul70322@gmail.com>
2022-07-24 01:29:06 +0530

    props passing implemented

--- solid-ecommerce/src/App.tsx
+++ solid-ecommerce/src/App.tsx
@@ -1,7 +1,46 @@
-import { Component } from "solid-js";
+import { Route, Routes } from 'solid-app-router';
+import { Component, createResource, createSignal } from 'solid-js';
+
+import { Header } from './components/Header';
+import { HomePage } from './components/HomePage';
+import { Product } from './product';
 
 const App: Component = () => {
-  return <div>Hello there from Tailwind and Typescript.</div>;
+  const [search, setSearch] = createSignal("");
+  const [cart, setCart] = createSignal<Product[]>([]);
+  const [products] = createResource<Product[]>(
+    async () => {
+      const res = await fetch("http://fakestoreapi.com/products");
+      return await res.json();
+    },
+    {
+      initialValue: [],
+    }
+  );
+  return (
+    <div>
+      <Header
+        cart={cart}
+        onClearCart={() => {
+          setCart([]);
+        }}
+        search={search}
+        onSetSearch={(str) => setSearch(str)}
+      />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <HomePage
+              products={products}
+              search={search}
+              onAddToCart={(p) => setCart([...cart(), p])}
+            />
+          }
+        />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
---
+++ solid-ecommerce/src/components/Header.tsx
@@ -0,0 +1,70 @@
+import { Link } from 'solid-app-router';
+import { Accessor, Component, createMemo, For } from 'solid-js';
+
+import type { Product } from "../product";
+
+export const Header: Component<{
+  cart: Accessor<Product[]>;
+  onClearCart: () => void;
+  search: Accessor<string>;
+  onSetSearch: (search: string) => void;
+}> = ({ cart, onClearCart, search, onSetSearch }) => {
+  const total = createMemo(() =>
+    cart ? cart().reduce((total, p) => total + p.price, 0) : 0
+  );
+
+  return (
+    <div class="bg-blue-900 text-white flex flex-row w-full py-4">
+      <div class="text-2xl px-10 py-2">
+        <Link href="/">
+          <i class="fas fa-donate mr-2"></i>
+          Simple eCommerce
+        </Link>
+      </div>
+      <div class="flex-grow">
+        <input
+          type="text"
+          value={search()}
+          onkeyup={(evt) => onSetSearch(evt.currentTarget.value)}
+          class="p-2 text-xl bg-white text-black rounded-lg max-w-md w-96"
+        />
+      </div>
+      <div class="px-10 py-2 justify-end has-tooltip">
+        <span class="tooltip cart">
+          <div>Cart ({(cart() ?? []).length})</div>
+          <For each={cart()}>
+            {(p) => (
+              <div class="flex flex-row my-2">
+                <img src={p.image} alt={p.title} class="h-8 mr-2" />
+                <h3 class="title text-md truncate flex-grow">{p.title}</h3>
+                <div class="text-md text-right flex-grow justify-end ml-2">
+                  {p.price.toLocaleString("en-US", {
+                    style: "currency",
+                    currency: "USD",
+                  })}
+                </div>
+              </div>
+            )}
+          </For>
+
+          <div class="flex">
+            <button
+              onClick={onClearCart}
+              class="text-md px-8 py-1 font-bold bg-blue-800 text-white rounded-full"
+            >
+              Clear Cart
+            </button>
+            <div class="text-md text-right flex-grow justify-end ml-2">
+              {total().toLocaleString("en-US", {
+                style: "currency",
+                currency: "USD",
+              })}
+            </div>
+          </div>
+        </span>
+        <i class="fas fa-shopping-cart mr-2"></i>
+        <span class="font-bold text-xl">{(cart() ?? []).length}</span>
+      </div>
+    </div>
+  );
+};
